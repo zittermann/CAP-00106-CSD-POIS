c288a6a7e20a49e197c6281ae498906e1982462e
Alejandro Giorgi <agiorgi@inrule.com>
2021-05-18 14:51:26 -0300

    Config inicial

---
+++ src/main/java/ar/com/grupoesfera/csd/pois/PoisApplication.java
@@ -0,0 +1,13 @@
+package ar.com.grupoesfera.csd.pois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/grupoesfera/csd/pois/PoisApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.grupoesfera.csd.pois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
