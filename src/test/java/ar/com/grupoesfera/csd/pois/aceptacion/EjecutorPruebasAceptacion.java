295e4b3d4154847e83ce50cd7ac638352b9e81c0
Alejandro Giorgi <agiorgi@inrule.com>
2021-06-01 10:25:33 -0300

    Ejecutor de pruebas de aceptacion. Falta configurar bien serenity con junit5

---
+++ src/test/java/ar/com/grupoesfera/csd/pois/aceptacion/EjecutorPruebasAceptacion.java
@@ -0,0 +1,8 @@
+package ar.com.grupoesfera.csd.pois.aceptacion;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+public class EjecutorPruebasAceptacion {
+}
