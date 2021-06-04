409f8faefac618cdf7e915e05ca012bae9d1ff31
Alejandro Giorgi <agiorgi@inrule.com>
2021-06-04 09:48:46 -0300

    Configuracion prueba de aceptacion HealthCheck

--- src/test/java/ar/com/grupoesfera/csd/pois/aceptacion/EjecutorPruebasAceptacion.java
+++ src/test/java/ar/com/grupoesfera/csd/pois/aceptacion/EjecutorPruebasAceptacion.java
@@ -1,10 +1,15 @@
 package ar.com.grupoesfera.csd.pois.aceptacion;
 
+import ar.com.grupoesfera.csd.pois.PoisApplication;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources")
+@CucumberOptions(features = "src/test/resources",
+                 glue = "ar.com.grupoesfera.csd.pois.aceptacion")
+                 //extraGlue = "ar.com.grupoesfera.csd.pois.aceptacion.pasos.configuraciones")
+@SpringBootTest(classes = PoisApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class EjecutorPruebasAceptacion {
 }
--- src/test/java/ar/com/grupoesfera/csd/pois/aceptacion/pasos/PedidosHTTPPasos.java
+++ src/test/java/ar/com/grupoesfera/csd/pois/aceptacion/pasos/PedidosHTTPPasos.java
@@ -1,33 +1,22 @@
 package ar.com.grupoesfera.csd.pois.aceptacion.pasos;
 
-import io.cucumber.java.Before;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.javacrumbs.jsonunit.core.Option;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static net.javacrumbs.jsonunit.spring.JsonUnitResultMatchers.json;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class PedidosHTTPPasos extends ConfiguracionContexto{
+public class PedidosHTTPPasos {
 
     @Autowired
-    private WebApplicationContext contexto;
-
     private MockMvc mockMvc;
-    private ResultActions resultado;
 
-    @Before
-    public void setup() {
-        mockMvc = MockMvcBuilders
-                .webAppContextSetup(contexto)
-                .build();
-    }
+    private ResultActions resultado;
 
     @When("el cliente hace un GET a {string}")
     public void elClienteHaceUnGETA(String ruta) throws Exception {
