a5275064b1851f7c944ab4d0a71e9c0482efebdb
Alejandro Giorgi <agiorgi@inrule.com>
2021-06-05 21:57:11 -0300

    Cuando no hay un poi cercano responde 204, sino responde un poi

---
+++ src/main/java/ar/com/grupoesfera/csd/pois/servicios/ServicioDePoi.java
@@ -0,0 +1,14 @@
+package ar.com.grupoesfera.csd.pois.servicios;
+
+import ar.com.grupoesfera.csd.pois.modelos.Poi;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ServicioDePoi {
+
+    public Optional<Poi> obtenerPoiMasCercano(Double latitud, Double longitud) {
+        return null;
+    }
+}
