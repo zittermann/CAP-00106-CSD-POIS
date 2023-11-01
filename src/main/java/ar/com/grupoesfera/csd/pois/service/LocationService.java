4e6175dbd94fd68bad84f94f1bf7630921c16b79
German Zittermann <zitterman@lacaja.com.ar>
2023-11-01 11:32:52 -0300

    Basic services created

---
+++ src/main/java/ar/com/grupoesfera/csd/pois/service/LocationService.java
@@ -0,0 +1,55 @@
+package ar.com.grupoesfera.csd.pois.service;
+
+import ar.com.grupoesfera.csd.pois.exceptions.NoNearPointFoundException;
+import ar.com.grupoesfera.csd.pois.modelos.Location;
+import ar.com.grupoesfera.csd.pois.repositorio.LocationRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static ar.com.grupoesfera.csd.pois.helpers.LocationHelper.calculateDistance;
+
+@Service
+public class LocationService {
+
+    private LocationRepository repository;
+
+    public LocationService(LocationRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Location> findAll() {
+        return repository.findAll();
+    }
+
+    public Location findNearestLocation(Double lat, Double lon, String label) {
+
+        List<Location> locations = repository.findAllByLabel(label);
+
+        Location nearestInterestPoint = null;
+        double closestLocation = 5; // in km, cannot bet longer than 5km
+
+        for(Location location: locations) {
+            double distance = calculateDistance(lat, lon,
+                    location.getLatitude(), location.getLongitude());
+
+            if (distance < closestLocation) {
+                closestLocation = distance;
+                nearestInterestPoint = location;
+            }
+
+        }
+
+        if (closestLocation == 5 ) // Significa que ningún punto está más cerca de 5km
+            throw new NoNearPointFoundException("No se ha encontrado un puntoc cercano");
+
+
+        return nearestInterestPoint;
+
+    }
+
+    public Location create(Location location) {
+        return repository.save(location);
+    }
+
+}
