07e54d9bc8a53bcdbf19430680df567d264ce517
German Zittermann <zitterman@lacaja.com.ar>
2023-11-01 11:30:37 -0300

    Find by label method created

---
+++ src/main/java/ar/com/grupoesfera/csd/pois/repositorio/LocationRepository.java
@@ -0,0 +1,14 @@
+package ar.com.grupoesfera.csd.pois.repositorio;
+
+import ar.com.grupoesfera.csd.pois.modelos.Location;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LocationRepository extends JpaRepository<Location, Long> {
+
+    List<Location> findAllByLabel(String label);
+
+}
