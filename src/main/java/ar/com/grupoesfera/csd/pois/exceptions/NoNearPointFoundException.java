e78ccdb668e8f260431c93e7126b2bc7fc3b4c68
German Zittermann <zitterman@lacaja.com.ar>
2023-11-01 11:28:55 -0300

    NotNearestPointFoundException created

---
+++ src/main/java/ar/com/grupoesfera/csd/pois/exceptions/NoNearPointFoundException.java
@@ -0,0 +1,7 @@
+package ar.com.grupoesfera.csd.pois.exceptions;
+
+public class NoNearPointFoundException extends RuntimeException {
+    public NoNearPointFoundException(String message) {
+        super(message);
+    }
+}
