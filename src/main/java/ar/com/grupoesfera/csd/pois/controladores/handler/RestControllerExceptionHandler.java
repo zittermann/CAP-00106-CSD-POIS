50c56cefa2f856013b5a0cdec5af60313e24c911
German Zittermann <zitterman@lacaja.com.ar>
2023-11-08 12:35:50 -0300

    When invalid lon or lat are sent now we show a custom error message

--- src/main/java/ar/com/grupoesfera/csd/pois/controladores/handler/RestControllerExceptionHandler.java
+++ src/main/java/ar/com/grupoesfera/csd/pois/controladores/handler/RestControllerExceptionHandler.java
@@ -3,9 +3,11 @@ package ar.com.grupoesfera.csd.pois.controladores.handler;
 import ar.com.grupoesfera.csd.pois.exceptions.NoNearPointFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
@@ -13,6 +15,9 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class RestControllerExceptionHandler {
+
+    // Excepción para BAD_REQUEST en RequestParam cuando parsea
+    // Harcodear mensaje
 
     @ExceptionHandler(NoNearPointFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
@@ -25,6 +30,34 @@ public class RestControllerExceptionHandler {
         body.put("error", HttpStatus.NOT_FOUND.toString());
 
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ResponseEntity<Object> handleMessageNotReadable(
+            RuntimeException ex) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", "La latitud y la longitud deben ser valores numéricos");
+        body.put("error", HttpStatus.BAD_REQUEST.toString());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ResponseEntity<Object> handleArgumentTypeMismatch(
+            RuntimeException ex) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", "La latitud y la longitud deben ser valores numéricos");
+        body.put("error", HttpStatus.BAD_REQUEST.toString());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
 
     }
 
