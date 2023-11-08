0218599678cf177db5b78872a0da7d82370e06df
German Zittermann <zitterman@lacaja.com.ar>
2023-11-08 12:49:23 -0300

    Test file initializated but empty

---
+++ src/test/java/ar/com/grupoesfera/csd/pois/unitarias/servicios/LocationServiceTest.java
@@ -0,0 +1,8 @@
+package ar.com.grupoesfera.csd.pois.unitarias.servicios;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class LocationServiceTest {
+}
